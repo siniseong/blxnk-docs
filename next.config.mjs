42cbee44fca112623fdcf49e7371473b54edf551
siniseong <shinhuiseong07@gmail.com>
2025-06-04 09:08:51 +0900

    build :: 빌드 명령어 수정

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const withNextra = nextra({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
   defaultShowCopyCode: true,
+  output: 'export',
   async redirects() {
     return [
       {
